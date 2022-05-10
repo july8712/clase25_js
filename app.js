09d235b1e478ade151f86d13ca3254fd6ebfe8ac
july8712 <julietabondi@gmail.com>
2022-05-10 13:08:01 -0300

    Dirección completa

--- app.js
+++ app.js
@@ -51,3 +51,14 @@
 
 // ----------------------------------------------------------------
 
+// Dirección completa
+
+// Crear un programa que pida al usuario ingresar, por separado, la calle, el número, el departamento, el código postal, la ciudad y el país, y muestre un mensaje con toda la dirección completa, p.ej.: "La dirección que ha ingresado es: Calle Falsa 123 3C, 1414, CABA, Argentina".
+
+const calle = prompt("Ingrese el nombre de la calle");
+const numero = prompt("Ingrese el número");
+const departamento = prompt("Ingrese el departamento");
+const cp = prompt("Ingrese el código postal");
+const ciudad = prompt("Ingrese la ciudad");
+const pais = prompt("Ingrese el país");
+console.log(`La dirección que ha ingresado es: ${calle} ${numero} ${departamento}, CP. ${cp}, ${ciudad}, ${pais}.`);

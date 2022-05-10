6da151d3a294edf66a5e6dbc989c68530cf31a81
july8712 <julietabondi@gmail.com>
2022-05-10 13:19:59 -0300

    Años perro

--- app.js
+++ app.js
@@ -55,10 +55,19 @@
 
 // Crear un programa que pida al usuario ingresar, por separado, la calle, el número, el departamento, el código postal, la ciudad y el país, y muestre un mensaje con toda la dirección completa, p.ej.: "La dirección que ha ingresado es: Calle Falsa 123 3C, 1414, CABA, Argentina".
 
-const calle = prompt("Ingrese el nombre de la calle");
-const numero = prompt("Ingrese el número");
-const departamento = prompt("Ingrese el departamento");
-const cp = prompt("Ingrese el código postal");
-const ciudad = prompt("Ingrese la ciudad");
-const pais = prompt("Ingrese el país");
-console.log(`La dirección que ha ingresado es: ${calle} ${numero} ${departamento}, CP. ${cp}, ${ciudad}, ${pais}.`);
+// const calle = prompt("Ingrese el nombre de la calle");
+// const numero = prompt("Ingrese el número");
+// const departamento = prompt("Ingrese el departamento");
+// const cp = prompt("Ingrese el código postal");
+// const ciudad = prompt("Ingrese la ciudad");
+// const pais = prompt("Ingrese el país");
+// console.log(`La dirección que ha ingresado es: ${calle} ${numero} ${departamento}, CP. ${cp}, ${ciudad}, ${pais}.`);
+
+// ----------------------------------------------------------------
+
+// Años perro
+
+// Crear un programa que pida ingresar una edad y devuelva el equivalente de esas edad en años perro.
+
+const edad = prompt("Ingrese la edad del perro");
+console.log(`Su perro tiene ${edad*7}`);

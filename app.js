17fff8384f8ebe91f9761484f1acb86160a7e316
july8712 <julietabondi@gmail.com>
2022-05-10 12:59:31 -0300

    Lista de reproducción

--- app.js
+++ app.js
@@ -41,3 +41,13 @@
 // Lista de reproducción
 
 // Crear un programa que pida al usuario ingresar, por separado, el nombre de una playlist y el título de tres canciones, y al finalizar se muestre un mensaje que diga, p. ej.: "Se ha creado la playlist 'Hits de los 80s' con las canciones 'Africa', 'Maniac', 'Final Countdown'".
+
+// const playlist = prompt("Ingrese el nombre de la playlist");
+// const cancion1 = prompt("Ingrese la primer canción");
+// const cancion2 = prompt("Ingrese la segunda canción");
+// const cancion3 = prompt("Ingrese la tercer canción");
+// console.log(`Se ha creado la playlist "${playlist}" con las canciones "${cancion1}", "${cancion2}" y "${cancion3}".`);
+
+
+// ----------------------------------------------------------------
+
